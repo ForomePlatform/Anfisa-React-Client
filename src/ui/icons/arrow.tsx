4366ad6ec9ebfd45aafa225cbdbdc86202507b73
Roman Basharin <roman.basharin@quantori.com>
2021-06-08 00:50:58 +0300

    feat new download buttons

--- src/ui/icons/arrow.tsx
+++ src/ui/icons/arrow.tsx
@@ -3,9 +3,27 @@ import { CSSProperties, ReactElement } from 'react'
 interface Props {
   fill?: string
   style?: CSSProperties
+  direction?: 'top' | 'right' | 'down' | 'left'
 }
 
-export const ArrowSvg = ({ fill, style }: Props): ReactElement => {
+export const ArrowSvg = ({ fill, style, direction }: Props): ReactElement => {
+  let transform = ''
+
+  switch (direction) {
+    case 'top':
+      transform = 'rotate(90deg)'
+      break
+    case 'right':
+      transform = 'rotate(180deg)'
+      break
+    case 'down':
+      transform = 'rotate(270deg)'
+      break
+    default:
+      transform = ''
+      break
+  }
+
   return (
     <svg
       width="16"
@@ -13,7 +31,7 @@ export const ArrowSvg = ({ fill, style }: Props): ReactElement => {
       viewBox="0 0 16 16"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
-      style={style}
+      style={{ ...style, transform }}
     >
       <path
         d="M10 4L6 8L10 12"
