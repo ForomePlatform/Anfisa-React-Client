867ced8e5d42f07faa407709a6176d8b9710676c
Vladislav <vladislav.lopatin@bravedevelopers.com>
2021-10-14 17:27:07 +0400

    fix(FOROME-208): getting data on ws-page
    fix(FOROME-208): getting data on ws-page

--- src/store/dataset.ts
+++ src/store/dataset.ts
@@ -389,7 +389,7 @@ class DatasetStore {
     })
   }
 
-  async fetchWsListAsync(isXL?: boolean, activePreset?: string) {
+  async fetchWsListAsync(isXL?: boolean) {
     const body = new URLSearchParams({
       ds: this.datasetName,
     })
@@ -399,7 +399,7 @@ class DatasetStore {
       body.append('zone', JSON.stringify(this.zone))
     }
 
-    activePreset && body.append('filter', activePreset)
+    this.activePreset && body.append('filter', this.activePreset)
 
     const response = await fetch(getApiUrl(isXL ? `ds_list` : `ws_list`), {
       method: 'POST',
