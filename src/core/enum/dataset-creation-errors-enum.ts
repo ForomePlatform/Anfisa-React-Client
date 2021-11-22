612b79f6a5f5856ac7e8a350799fee0e2c289165
Vladislav <vladislav.lopatin@bravedevelopers.com>
2021-11-22 12:21:03 +0400

    feat(FOROME-306): add drawer tags validation

--- src/core/enum/dataset-creation-errors-enum.ts
+++ src/core/enum/dataset-creation-errors-enum.ts
@@ -1,6 +1,6 @@
 export enum DatasetCreationErrorsEnum {
   EmptyDataset = 'Dataset is empty',
   IncorrectName = 'Incorrect dataset name',
-  DatasetExists = 'Such a name of dataset already exist',
+  DatasetExists = `That dataset's name is taken. Try another`,
   ChooseAnyFilter = 'Choose any Filter Preset or Filter Refiner condition',
 }
