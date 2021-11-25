37185a43551931d5a83fe1707fa77c0e55fc922a
evgeniy-chernomortsev <evgeniy.chernomortsev@quantori.com>
2021-11-25 17:09:57 +0300

    feat(FOROME-301) add refiner search params url + opened variant

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,6 +3,7 @@ import { Option } from 'react-dropdown'
 import { Link, useHistory } from 'react-router-dom'
 import { toast } from 'react-toastify'
 import get from 'lodash/get'
+import { toJS } from 'mobx'
 import { observer } from 'mobx-react-lite'
 
 import { copyToClipboard } from '@core/copy-to-clipboard'
@@ -35,15 +36,15 @@ export const Header = observer(
         await dirinfoStore.fetchDirInfoAsync()
 
         const xlName = get(
-          dirinfoStore,
-          `dirinfo['ds-dict'][${ds}].ancestors[0][0]`,
+          toJS(dirinfoStore.dirinfo),
+          `['ds-dict'][${ds}].ancestors[0][0]`,
           '',
         )
 
         setXlDatasetName(xlName)
 
         setDatasets(
-          get(dirinfoStore, `dirinfo.ds-dict.${xlName}.secondary`, []),
+          get(toJS(dirinfoStore.dirinfo), `ds-dict.${xlName}.secondary`, []),
         )
       }
 
@@ -90,7 +91,7 @@ export const Header = observer(
               <Logo mode="white" className="mr-4" />
 
               <span className="text-grey-blue">
-                {dirinfoStore.dirinfo.version as string}
+                {toJS(dirinfoStore.dirinfo).version as string}
               </span>
             </div>
           </Link>
--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,6 +1,7 @@
 import { ReactElement } from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify'
+import { QueryParamProvider } from 'use-query-params'
 
 import { GlobalStyle } from '@theme'
 import { Routes } from '@router/routes.enum'
@@ -10,14 +11,16 @@ import { WSPage } from '@pages/ws/page'
 
 export const RouterBase = (): ReactElement => (
   <Router>
-    <GlobalStyle />
-    <ToastContainer />
+    <QueryParamProvider ReactRouterRoute={Route}>
+      <GlobalStyle />
+      <ToastContainer />
 
-    <Switch>
-      <Route path={Routes.Root} exact component={MainPage} />
-      <Route path={Routes.WS} exact component={WSPage} />
-      <Route path={Routes.Filter} exact component={FilterPage} />
-      <Route path={Routes.Refiner} exact component={FilterPage} />
-    </Switch>
+      <Switch>
+        <Route path={Routes.Root} exact component={MainPage} />
+        <Route path={Routes.WS} exact component={WSPage} />
+        <Route path={Routes.Filter} exact component={FilterPage} />
+        <Route path={Routes.Refiner} exact component={FilterPage} />
+      </Switch>
+    </QueryParamProvider>
   </Router>
 )
