05312114f7b610a35d1d3d7cb029da05e4b28f99
Vladislav <vladislav.lopatin@bravedevelopers.com>
2021-12-13 15:34:42 +0400

    fix(FOROME-427): divide search blocks on dtree page

--- src/store/dtree.ts
+++ src/store/dtree.ts
@@ -70,6 +70,10 @@ class DtreeStore {
 
   isFilterContentExpanded = false
   filterChangeIndicator = 0
+
+  isFilterModalContentExpanded = false
+  filterModalChangeIndicator = 0
+
   isFiltersLoading = false
   isDtreeLoading = false
 
@@ -77,6 +81,7 @@ class DtreeStore {
   resultsChangeIndicator = 0
 
   filterValue = ''
+  filterModalValue = ''
   algorithmFilterValue = ''
   filteredCounts = 0
 
@@ -223,6 +228,10 @@ class DtreeStore {
 
   get getQueryBuilder() {
     return getQueryBuilder(toJS(this.dtreeStat['stat-list']))
+  }
+
+  get getModalQueryBuilder() {
+    return getQueryBuilder(toJS(this.dtreeStat['stat-list']), 'modal')
   }
 
   getStepIndexForApi = (index: number) => {
@@ -851,8 +860,14 @@ class DtreeStore {
     this.filterChangeIndicator--
   }
 
-  resetFilterChangeIndicator() {
-    this.filterChangeIndicator = 0
+  expandFilterModalContent() {
+    this.isFilterModalContentExpanded = true
+    this.filterModalChangeIndicator++
+  }
+
+  collapseFilterModalContent() {
+    this.isFilterModalContentExpanded = false
+    this.filterModalChangeIndicator--
   }
 
   expandResultsContent() {
@@ -865,16 +880,28 @@ class DtreeStore {
     this.resultsChangeIndicator--
   }
 
-  resetResultsChangeIndicator() {
-    this.resultsChangeIndicator = 0
-  }
-
   setFilterValue(item: string) {
     this.filterValue = item
   }
 
+  resetFilterValue() {
+    this.filterValue = ''
+  }
+
+  setFilterModalValue(item: string) {
+    this.filterModalValue = item
+  }
+
+  resetFilterModalValue() {
+    this.filterModalValue = ''
+  }
+
   setAlgorithmFilterValue(item: string) {
     this.algorithmFilterValue = item
+  }
+
+  resetAlgorithmFilterValue() {
+    this.algorithmFilterValue = ''
   }
 
   setIsFiltersLoading() {
