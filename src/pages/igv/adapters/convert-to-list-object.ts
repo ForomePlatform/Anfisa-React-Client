9007ad14d3bfbc7f905c5010049e7e8a3cfb6e45
Yaroslav Trefilov <yaroslav.trefilov@bravedevelopers.com>
2022-02-15 15:19:19 +0400

    fix (FOROME-547): change rout for bucket with bum files

--- src/pages/igv/adapters/convert-to-list-object.ts
+++ src/pages/igv/adapters/convert-to-list-object.ts
@@ -1,3 +1,4 @@
+import { hg38Folder } from '../igv.page'
 import { IListBucket } from '../types/list-bucket.interface'
 
 export const convertToListObject = (listBucket: IListBucket): string[] => {
@@ -8,7 +9,7 @@ export const convertToListObject = (listBucket: IListBucket): string[] => {
   contents.forEach(element => {
     const name: string = element.Key._text
 
-    if (name.startsWith('bams/')) listObject.push(name)
+    if (name.startsWith(hg38Folder)) listObject.push(name)
   })
 
   return listObject
