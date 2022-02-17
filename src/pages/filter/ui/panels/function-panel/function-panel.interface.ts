0e28e5ab9617b45266415ceab216af7ba2edff67
Vladislav <Lapk1n@yandex.ru>
2022-02-17 15:04:33 +0400

    fix(FOROME-660): custom inheritance mode

--- src/pages/filter/ui/panels/function-panel/function-panel.interface.ts
+++ src/pages/filter/ui/panels/function-panel/function-panel.interface.ts
@@ -8,3 +8,10 @@ export interface IInheritanceModeCachedValues {
   conditions: { problem_group: string[] }
   variants: string[]
 }
+
+export type TScenario = [string, string[]]
+
+export interface ICustomInheritanceModeCachedValues {
+  conditions: { scenario: TScenario[] }
+  reset: string
+}
