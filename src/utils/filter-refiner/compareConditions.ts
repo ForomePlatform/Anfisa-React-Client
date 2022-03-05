48b73c3d038f5924a8eb9a30438f75ec66dfef14
Vladislav <Lapk1n@yandex.ru>
2022-03-05 19:26:24 +0400

    feat(FOROME-363): compare conditions fixes

--- src/utils/filter-refiner/compareConditions.ts
+++ src/utils/filter-refiner/compareConditions.ts
@@ -1,10 +1,11 @@
 import difference from 'lodash/difference'
+import { toJS } from 'mobx'
 
 import { Condition } from '@store/dataset'
 
 interface ICompareConditionsProps {
   currentConditions: Condition[]
-  startConditions: Condition[]
+  startConditions: any[]
   currentPreset?: string
   prevPreset?: string
 }
@@ -15,21 +16,39 @@ export const compareConditions = ({
   currentPreset,
   prevPreset,
 }: ICompareConditionsProps): boolean => {
+  if (!currentPreset) return false
+
   if (currentConditions.length !== startConditions.length) return true
 
   if (prevPreset && currentPreset !== prevPreset) return true
 
   const sum = currentConditions.reduce(
     (acc, condition: any[], index): number => {
-      if (difference(condition[index], startConditions[index]).length === 0) {
+      if (difference(condition, startConditions[index]).length === 0) {
         acc += 1
       }
       return acc
     },
     0,
   )
+  // TODO:
+  // const someResult = currentConditions.every((condition: any[], index) => {
+  //   console.log('loop')
+
+  //   if (
+  //     difference(condition[index], startConditions[index]).length === 0 &&
+  //     condition[condition.length - 1].length ===
+  //       startConditions[startConditions.length - 1].length
+  //   ) {
+  //     return false
+  //   }
+  //   return true
+  // })
+
+  // console.log('result from reduce:', sum !== currentConditions.length)
+  // console.log('result from every:', someResult)
 
   if (sum === currentConditions.length) return false
 
-  return false
+  return true
 }
