80152359a68bd39542da10bf388370c089a49edd
Vladislav <Lapk1n@yandex.ru>
2022-03-10 15:33:54 +0400

    refactor(FOROME-722): modals inheritance mode

--- src/pages/filter/ui/modal-edit/modal-edit.store.ts
+++ src/pages/filter/ui/modal-edit/modal-edit.store.ts
@@ -1,7 +1,18 @@
 import { makeAutoObservable } from 'mobx'
 
+import { StatList } from '@declarations'
+import dtreeStore from '@store/dtree'
 import activeStepStore from '@pages/filter/active-step.store'
 import dtreeModalStore from '../../modals.store'
+
+export const selectOptions = ['--', '0', '0-1', '1', '1-2', '2']
+export interface IParams {
+  approx: any
+  state?: string[] | null
+  default?: string
+  request?: any[]
+}
+
 class ModalEditStore {
   constructor() {
     makeAutoObservable(this)
@@ -13,6 +24,87 @@ class ModalEditStore {
 
     const location: [number, number] = [+stepIndexForApi, locationIndex]
     return location
+  }
+
+  public get variants(): string[] | [string, number] {
+    return dtreeStore.statFuncData.variants
+  }
+
+  public get currentStepIndex(): number {
+    return activeStepStore.activeStepIndex
+  }
+
+  public get currentGroupIndex(): number {
+    return dtreeModalStore.groupIndexToChange
+  }
+
+  public get currentGroup(): any[] {
+    return dtreeStore.stepData[activeStepStore.activeStepIndex].groups[
+      this.currentGroupIndex
+    ]
+  }
+
+  public get groupName(): string {
+    return dtreeModalStore.groupNameToChange
+  }
+
+  public get selectedGroupsAmount(): string[] {
+    return this.currentGroup && this.currentGroup.length > 0
+      ? dtreeStore.selectedFilters
+      : []
+  }
+
+  public get currentGroupLength(): number {
+    return dtreeStore.stepData[this.currentStepIndex].groups[
+      this.currentGroupIndex
+    ].length
+  }
+
+  public get currentStepGroups(): string[] {
+    return dtreeStore.currentStepGroups
+  }
+
+  public get statList(): StatList[] {
+    return dtreeStore.startDtreeStat['stat-list']
+  }
+
+  public get problemGroups(): string[] {
+    return (
+      this.statList.find((item: any) => item.name === this.groupName)?.family ??
+      []
+    )
+  }
+
+  public get approxModes() {
+    return (
+      this.statList.find((item: any) => item.name === this.groupName)?.[
+        'approx-modes'
+      ] ?? []
+    )
+  }
+
+  public get approxOptions(): string[] {
+    const approxOptions: string[] = []
+
+    this.approxModes.map((mode: string[]) => {
+      approxOptions.push(mode[1])
+    })
+
+    return approxOptions
+  }
+
+  public get approxValues(): string[] {
+    const approxValues: string[] = []
+
+    this.approxModes.map((mode: string[]) => {
+      approxValues.push(mode[0])
+    })
+
+    return approxValues
+  }
+
+  public openModalJoin(): void {
+    dtreeModalStore.openModalJoin()
   }
 }
 
