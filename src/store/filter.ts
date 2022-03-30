55f2314e73265d474603f71839849ad5aa757041
Vladislav <Lapk1n@yandex.ru>
2022-03-30 21:42:23 +0400

    feat(FOROME-791): removed adding active id after preset loading

--- src/store/filter.ts
+++ src/store/filter.ts
@@ -77,8 +77,6 @@ export class FilterStore {
     const filterId: string = nanoid()
 
     this._selectedFilters.set(filterId, condition)
-
-    this.setActiveFilterId(filterId)
   }
 
   public removeFilterBlock(filterId: string): void {
