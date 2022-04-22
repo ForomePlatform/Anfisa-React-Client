9b4fd5f445bfadb201fe880bf2ab647b15014d8d
Vladislav <Lapk1n@yandex.ru>
2022-04-22 15:53:53 +0400

    fix(FOROME-904): add hook to define client height

--- src/pages/filter/ui/empty-selected-group.tsx
+++ src/pages/filter/ui/empty-selected-group.tsx
@@ -1,20 +1,28 @@
-import { ReactElement } from 'react'
+import { ReactElement, useRef } from 'react'
 import cn, { Argument } from 'classnames'
 
+import useClientHeight from '@core/hooks/use-client-height'
 import { t } from '@i18n'
 
 type Props = {
   className?: Argument
 }
-export const EmptySelectedGroup = ({ className }: Props): ReactElement => (
-  <div className={cn('w-1/3', className)}>
-    <div
-      className="flex items-center justify-center border border-grey-disabled"
-      style={{ height: 'calc(100vh - 197px)' }}
-    >
-      <p className="leading-16px text-grey-blue align-center">
-        {t('dtree.selectAttribute')}
-      </p>
+export const EmptySelectedGroup = ({ className }: Props): ReactElement => {
+  const emptyDivkRef = useRef<any>()
+
+  const emptyBlockHeight = useClientHeight(emptyDivkRef)
+
+  return (
+    <div className={cn('w-1/3', className)}>
+      <div
+        ref={emptyDivkRef}
+        className="flex items-center justify-center border border-grey-disabled"
+        style={{ height: emptyBlockHeight }}
+      >
+        <p className="leading-16px text-grey-blue align-center">
+          {t('dtree.selectAttribute')}
+        </p>
+      </div>
     </div>
-  </div>
-)
+  )
+}
