91fd63a278c09c7eb4b1da495c046d9864b9fada
Andrey Ustyumenko <andrey.ustyumenko@quantori.com>
2022-05-04 02:43:44 +0400

    fix: fixed eslint

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,5 @@
 const path = require('path')
-const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin')
 
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
@@ -14,20 +14,19 @@ module.exports = {
   core: {
     builder: '@storybook/builder-webpack5',
   },
-  webpackFinal: async (config, {configType}) => {
+  webpackFinal: async (config, { configType }) => {
     config.module.rules.push({
-        test: /\.scss|.sass$/,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
-        include: path.resolve(__dirname, '../'),
-    });
+      test: /\.scss|.sass$/,
+      use: ['style-loader', 'css-loader', 'sass-loader'],
+      include: path.resolve(__dirname, '../'),
+    })
 
     config.resolve.plugins = [
-        new TsconfigPathsPlugin({
-            configFile: path.resolve(__dirname, '../tsconfig.json')
-        }),
-    ];
+      new TsconfigPathsPlugin({
+        configFile: path.resolve(__dirname, '../tsconfig.json'),
+      }),
+    ]
 
-    // Return the altered config
-    return config;
-},
+    return config
+  },
 }
