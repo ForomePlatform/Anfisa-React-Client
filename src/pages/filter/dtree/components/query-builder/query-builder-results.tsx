97ab14cf2244fe653f9dde8209c981b8eff44793
alexander.malyshev <alexander.malyshev@quantori.com>
2022-05-10 15:23:37 +0500

    fix(FOROME-928): fixed display of accepted and rejected variants for WS datasets in decision tree

---
+++ src/pages/filter/dtree/components/query-builder/query-builder-results.tsx
@@ -0,0 +1,69 @@
+import { ReactElement } from 'react'
+import { observer } from 'mobx-react-lite'
+
+import { t } from '@i18n'
+import dtreeStore from '@store/dtree'
+import { Button } from '@ui/button'
+import { DecisionTreesResultsDataCy } from '@components/data-testid/decision-tree-results.cy'
+import { QueryBuilderResultsNumbers } from './query-builder-results-numbers'
+
+export const QueryBuilderResults = observer((): ReactElement => {
+  const { stepData, isTreeEmpty } = dtreeStore
+
+  const stepIndex = stepData.findIndex(
+    element => element.isActive || element.isReturnedVariantsActive,
+  )
+
+  const currentStep = stepData[stepIndex]
+
+  const hasReturnedVariants = currentStep?.returnPointIndex != null
+  const hasStartVariants = currentStep?.conditionPointIndex != null
+  const shouldShowReturnedVariants = hasReturnedVariants && !isTreeEmpty
+
+  const openTableModal = (isReturnedVariants = true) => {
+    const hasEmptyStep = stepData.some(element => {
+      if (!element.isFinalStep) {
+        return element.groups.length === 0
+      }
+      return false
+    })
+
+    const indexForApi = dtreeStore.getStepIndexForApi(stepIndex)
+    const nextStepIndex = isReturnedVariants ? indexForApi + 1 : indexForApi
+    const fixedNextStepIndex = hasEmptyStep ? nextStepIndex - 1 : nextStepIndex
+
+    dtreeStore.openModalViewVariants(fixedNextStepIndex)
+  }
+
+  return (
+    <div className="flex items-center p-4 border-b border-grey-light bg-blue-dark justify-between">
+      <div>
+        <div className="font-bold text-white">{t('dtree.results')}</div>
+
+        <div className="text-12 leading-14px text-grey-blue mt-2">
+          <QueryBuilderResultsNumbers />
+        </div>
+      </div>
+      <div className="flex">
+        {shouldShowReturnedVariants && (
+          <Button
+            dataTestId={DecisionTreesResultsDataCy.viewReturnedVariants}
+            onClick={() => openTableModal(true)}
+            text={t('dtree.viewReturnedVariants')}
+            variant="secondary-dark"
+            className="ml-auto"
+          />
+        )}
+
+        {hasStartVariants && (
+          <Button
+            onClick={() => openTableModal(false)}
+            text={t('dtree.viewVariants')}
+            variant="secondary-dark"
+            className="ml-5"
+          />
+        )}
+      </div>
+    </div>
+  )
+})
