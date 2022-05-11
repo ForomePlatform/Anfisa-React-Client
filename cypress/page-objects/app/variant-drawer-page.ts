6dbe4b1d9848f34ecdcc40e236de7b12b15d1d88
Alex Vershinin <alexander.vershinin@quantori.com>
2022-05-11 16:30:43 +0400

    chore(FOROME-982): add linter fixes

--- cypress/page-objects/app/variant-drawer-page.ts
+++ cypress/page-objects/app/variant-drawer-page.ts
@@ -1,7 +1,7 @@
 import { CommonSelectors } from '../../../src/components/data-testid/common-selectors.cy'
 import { VariantDrawerDataCy } from '../../../src/components/data-testid/variant-drawer.cy'
-import { BasePage } from '../lib/base-page'
 import { Helper } from '../../shared/helpers'
+import { BasePage } from '../lib/base-page'
 import { VariantDrawerWidget } from './widgets/variant-drawer.widget'
 
 class VariantDrawerPage extends BasePage {
