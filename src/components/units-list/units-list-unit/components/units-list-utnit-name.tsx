e56473767f3e0504344d4f2ba763cda00aa5440f
Andrey Ustyumenko <andrey.ustyumenko@quantori.com>
2022-05-26 23:38:42 +0300

    feat(FOROME-1079): Added tooltips on left panel

---
+++ src/components/units-list/units-list-unit/components/units-list-utnit-name.tsx
@@ -0,0 +1,33 @@
+import { FC, memo } from 'react'
+import Tooltip from 'rc-tooltip'
+
+export interface IUnitsListUnitNameProps {
+  className: string
+  name: string
+  tooltip?: string
+  onClick: () => void
+}
+
+// eslint-disable-next-line react/display-name
+export const UnitsListUnitName: FC<IUnitsListUnitNameProps> = memo(
+  ({ tooltip, name, className, onClick }) => {
+    const nameElement = (
+      <span className={className} onClick={onClick}>
+        {name}
+      </span>
+    )
+
+    return !tooltip ? (
+      nameElement
+    ) : (
+      <Tooltip
+        overlay={tooltip}
+        placement="topLeft"
+        trigger="hover"
+        mouseEnterDelay={0.7}
+      >
+        {nameElement}
+      </Tooltip>
+    )
+  },
+)
