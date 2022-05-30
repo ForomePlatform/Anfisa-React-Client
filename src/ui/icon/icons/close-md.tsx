ba6a9798f63f1e222371eb3326548fb8cf378d59
Andrey Ustyumenko <andrey.ustyumenko@quantori.com>
2022-05-31 00:31:32 +0300

    fix: cr fix

--- src/ui/icon/icons/close-md.tsx
+++ src/ui/icon/icons/close-md.tsx
@@ -1,5 +1,3 @@
-import { Fragment } from 'react'
-
 const size = 16
 
 export default {
@@ -7,9 +5,9 @@ export default {
   viewBox: { w: size, h: size },
   stroke: true,
   content: (
-    <Fragment>
+    <>
       <path d="M4 4L12 12" strokeWidth="1" strokeLinecap="round" />
       <path d="M12 4L4 12" strokeWidth="1" strokeLinecap="round" />
-    </Fragment>
+    </>
   ),
 }
