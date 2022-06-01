42e24cf6ec6b5ef30d710c70171abc1c25483984
alexander.malyshev <alexander.malyshev@quantori.com>
2022-06-01 09:08:50 +0500

    feat(FOROME-1051): pr fixes

--- src/components/variant-aspects-layout/variant-aspects-layout-grid.tsx
+++ src/components/variant-aspects-layout/variant-aspects-layout-grid.tsx
@@ -3,13 +3,15 @@ import GridLayout, { WidthProvider } from 'react-grid-layout'
 import cn from 'classnames'
 
 import { AspectWindow, TWindowToggleHandleParams } from './aspect-window'
-import { IVariantAspectsLayoutGridProps } from './variant-aspects-layout.interface'
 import {
-  adaptLayoutForAspects,
   GRID_LAYOUT_COLS,
   GRID_LAYOUT_CONTAINER_PADDING,
   GRID_LAYOUT_MARGIN,
   GRID_LAYOUT_ROW_HEIGHT,
+} from './variant-aspects-layout.constants'
+import { IVariantAspectsLayoutGridProps } from './variant-aspects-layout.interface'
+import {
+  adaptLayoutForAspects,
   maximizeWindow,
   minimizeWindow,
   useGridHandles,
