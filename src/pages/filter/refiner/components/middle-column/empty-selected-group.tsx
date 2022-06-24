a87ef33a55e3ee895ea8fb1b66948367ffe9e80b
QSdmitrioul <dmitriy.skroba@quantori.com>
2022-06-24 13:11:36 +0300

    feat(FOROME-1227): add loader in fr attributes

--- src/pages/filter/refiner/components/middle-column/empty-selected-group.tsx
+++ src/pages/filter/refiner/components/middle-column/empty-selected-group.tsx
@@ -4,16 +4,21 @@ import cn, { Argument } from 'classnames'
 import { t } from '@i18n'
 
 type IEmptySelectedGroupProps = {
+  content?: JSX.Element
   className?: Argument
 }
 export const EmptySelectedGroup = ({
+  content,
   className,
 }: IEmptySelectedGroupProps): ReactElement => {
   return (
     <div className={cn('flex items-center justify-center', className)}>
-      <p className="leading-16px text-grey-blue align-center">
-        {t('condition.selectAttribute')}
-      </p>
+      {!content && (
+        <p className="leading-16px text-grey-blue align-center">
+          {t('condition.selectAttribute')}
+        </p>
+      )}
+      {!!content && content}
     </div>
   )
 }
