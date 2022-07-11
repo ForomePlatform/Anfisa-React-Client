96a2287b9e2787e6a56b32214b3c18db8dcbd55b
Vladislav <Lapk1n@yandex.ru>
2022-07-11 12:32:07 +0400

    fix(FOROME-00): build crush

--- src/components/variant-aspects-layout/components/open-igv-popover.tsx
+++ src/components/variant-aspects-layout/components/open-igv-popover.tsx
@@ -6,7 +6,7 @@ import { Popover } from '@ui/popover'
 import { IPopoverBaseProps } from '@ui/popover/popover.interface'
 import { PopperMenu } from '@components/popper-menu/popper-menu'
 import { PopperMenuItem } from '@components/popper-menu/popper-menu-item'
-import { popoverOffset } from '@pages/ws/constants'
+import { popoverOffset } from '@pages/ws/ws.constants'
 
 interface IOpenIgvPopoverProps extends IPopoverBaseProps {
   onOpenModal: () => void
