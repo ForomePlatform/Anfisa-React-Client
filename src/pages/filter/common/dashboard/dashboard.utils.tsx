c3d3cc2dcb7a117f221c800d32e29226b81d7230
Vladislav <Lapk1n@yandex.ru>
2022-07-29 16:51:41 +0400

    feat(FOROME-1405): fix constants

--- src/pages/filter/common/dashboard/dashboard.utils.tsx
+++ src/pages/filter/common/dashboard/dashboard.utils.tsx
@@ -2,15 +2,24 @@ import { Layout } from 'react-grid-layout'
 import cloneDeep from 'lodash/cloneDeep'
 
 import {
+  DASHBOARD_COLS_OFFSET_HEIGHT,
+  DASHBOARD_LAYOUT_COLS,
+  DASHBOARD_LAYOUT_ROW_HEIGHT,
+  DASHBOARD_ROW_OFFSET_HEIGHT,
+  HIDDEN_RESIZE_HEIGHT,
+} from './dashboard.constants'
+import {
   IExtendedTUnitGroups,
   IGetLayoutOnHeightChange,
 } from './dashboard.interfaces'
 
 export const getStartLayout = (groups: IExtendedTUnitGroups[]): Layout[] => {
+  const cols = DASHBOARD_LAYOUT_COLS
+
   return groups.map((group, index) => ({
     i: group.name,
-    x: index < 4 ? index : index % 4,
-    y: index < 4 ? 0 : Math.floor(index / 4),
+    x: index < cols ? index : index % cols,
+    y: index < cols ? 0 : Math.floor(index / cols),
     w: 1,
     h: group.units.length + 1,
   }))
@@ -20,10 +29,12 @@ export const getNewTabLayout = (
   tabsLength: number,
   group: IExtendedTUnitGroups,
 ): Layout => {
+  const cols = DASHBOARD_LAYOUT_COLS
+
   return {
     i: group.name,
-    x: tabsLength < 4 ? tabsLength : tabsLength % 4,
-    y: tabsLength < 4 ? 0 : Math.floor(tabsLength / 4),
+    x: tabsLength < cols ? tabsLength : tabsLength % cols,
+    y: tabsLength < cols ? 0 : Math.floor(tabsLength / cols),
     w: 1,
     h: group.units.length + 1,
   }
@@ -47,7 +58,9 @@ export const getLayoutOnTabHeightChange = (
   }
 
   if (!isOpen && tabChildren) {
-    clonedLayout[index].h = (height - 12) / 44
+    clonedLayout[index].h =
+      (height + HIDDEN_RESIZE_HEIGHT - DASHBOARD_COLS_OFFSET_HEIGHT) /
+      DASHBOARD_ROW_OFFSET_HEIGHT
   } else if (isOpen && tabChildren) {
     clonedLayout[index].h = tabChildren.length - 1
   }
@@ -65,9 +78,14 @@ export const getLayoutOnSubTabHeightChange = (
   const clonedLayout = cloneDeep(mainTabsLayout)
 
   if (subTabHeight && !isOpen) {
-    clonedLayout[index].h = (subTabHeight - 36) / 44 + clonedLayout[index].h
+    clonedLayout[index].h =
+      (subTabHeight - DASHBOARD_LAYOUT_ROW_HEIGHT) /
+        DASHBOARD_ROW_OFFSET_HEIGHT +
+      clonedLayout[index].h
   } else if (subTabHeight && isOpen) {
-    clonedLayout[index].h = clonedLayout[index].h - (subTabHeight - 36) / 44
+    clonedLayout[index].h =
+      clonedLayout[index].h -
+      (subTabHeight - DASHBOARD_LAYOUT_ROW_HEIGHT) / DASHBOARD_ROW_OFFSET_HEIGHT
   }
 
   return clonedLayout
