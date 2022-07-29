5ccec28d135c88190366471ed5b80bb0d543ac84
nikita-kapishev <nikita.kapishev@quantori.com>
2022-07-30 00:24:16 +0500

    Revert "leave only URL_BACKEND environment"
    This reverts commit c6d368ae1506d1d8306d1b146c237ecde589123f.

--- docker/99-envconfigsubst.sh
+++ docker/99-envconfigsubst.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-envsubst '${URL_BACKEND}' < /usr/share/nginx/html/anfisa/env-config.js.template > /usr/share/nginx/html/anfisa/env-config.js
+envsubst '${REACT_APP_URL_BACKEND}' < /usr/share/nginx/html/anfisa/env-config.js.template > /usr/share/nginx/html/anfisa/env-config.js
