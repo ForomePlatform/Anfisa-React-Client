adc957c0afd5897122d391a904fd13a5ae1f9297
QSdmitrioul <100569351+QSdmitrioul@users.noreply.github.com>
2022-09-16 11:21:52 +0300

    fix(FOROME-1636): fix flow of wizard (#949)
    * fix(FOROME-1636): fix flow of wizard
    
    * fix(FOROME-1636): extract function

--- src/pages/main/components/selected-dataset/build-flow/selected-dataset-build-flow.tsx
+++ src/pages/main/components/selected-dataset/build-flow/selected-dataset-build-flow.tsx
@@ -3,6 +3,7 @@ import styles from './build-flow.module.css'
 import { ReactElement } from 'react'
 import { observer } from 'mobx-react-lite'
 
+import { IWizardScenario } from '@pages/main/components/selected-dataset/build-flow/components/wizard/wizard.interface'
 import { BuildFlowHeader } from './components/build-flow-header'
 import wizardStore from './components/wizard/wizard.store'
 
@@ -13,19 +14,35 @@ export const SelectedDatasetBuildFlow = observer(
       wizardStore.resetWizard()
     }
 
+    const renderCard = ({ component, ...rest }: IWizardScenario) => {
+      const Component = () =>
+        component({
+          ...rest,
+        })
+      return !rest.hidden && <Component key={rest.id} />
+    }
+
     return (
       <div className={styles.buildFlow}>
         <div className={styles.buildFlow__container}>
           <BuildFlowHeader goBack={handleGoBack} />
 
           <div>
-            {wizardStore.wizardScenario.map(({ component, ...rest }) => {
-              const Component = () =>
-                component({
-                  ...rest,
-                })
-              return !rest.hidden && <Component key={rest.id} />
-            })}
+            {wizardStore.wizardScenario
+              .filter(it => it.position !== 'left' && it.position !== 'right')
+              .map(renderCard)}
+            <div className="flex">
+              <div className="w-[calc(50%-8px)]">
+                {wizardStore.wizardScenario
+                  .filter(it => it.position === 'left')
+                  .map(renderCard)}
+              </div>
+              <div className="ml-4 w-[calc(50%-8px)]">
+                {wizardStore.wizardScenario
+                  .filter(it => it.position === 'right')
+                  .map(renderCard)}
+              </div>
+            </div>
           </div>
         </div>
       </div>
