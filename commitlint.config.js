f59a83f4a62149a08b6830fae9227d101b2f9eeb
Mike Chumack <mklchum@gmail.com>
2025-04-02 16:53:53 -0400

    build: remove "jira-ticket-number-in-commit requirement"

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,20 +1,3 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
-  plugins: ['commitlint-plugin-function-rules'],
-  rules: {
-    'function-rules/scope-case': [
-      2, // level: error
-      'always',
-      parsed => {
-        if (/FOROME-\d{2,}/.test(parsed.scope)) {
-          return [true]
-        }
-        return [
-          false,
-          'scope should contain Jira issue reference on the round brackets, like (FOROME-111)',
-        ]
-      },
-    ],
-  },
-  'react/no-namespace': 'off',
 }
